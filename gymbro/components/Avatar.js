91e7844a2858f08721f8daa8193500f949fafd4a
paramighty <avinnosatta@gmail.com>
2023-09-05 13:04:51 +0200

    Created supabase for post. Added dynamic Avatar
    Co-authored-by: MorganRapaport <MorganRapaport@users.noreply.github.com>
    Co-authored-by: RebekkaBaerr <RebekkaBaerr@users.noreply.github.com>

--- gymbro/components/Avatar.js
+++ gymbro/components/Avatar.js
@@ -1,6 +1,4 @@
-export default function Avatar({
-  size,
-}) {
+export default function Avatar({ size, url }) {
   let width = "w-12";
   if (size === "lg") {
     width = "w-24 md:w-36";
@@ -9,10 +7,7 @@ export default function Avatar({
     <div
       className={`${width} rounded-full flex overflow-hidden flex justify-center items-center`}
     >
-      <img
-        src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80"
-        alt=""
-      />
+      <img src={url} alt="" />
     </div>
   );
 }
--- gymbro/components/PostFormCard.js
+++ gymbro/components/PostFormCard.js
@@ -1,25 +1,59 @@
 import Card from "./card";
 import Avatar from "./Avatar";
+import { useEffect, useState } from "react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 
 export default function PostFormCard() {
+  const [profile, setProfile] = useState(null);
+  const [content, setContent] = useState();
+  const supabase = useSupabaseClient();
+  const session = useSession();
+
+  useEffect(() => {
+    supabase
+      .from("profiles")
+      .select()
+      .eq("id", session.user.id)
+      .then((result) => {
+        if (result.data.length) {
+          setProfile(result.data[0]); //do not understand this I
+        }
+      });
+  }, []);
+
+  function createPost() {
+    supabase
+      .from("posts")
+      .insert("author", session.user.id, content)
+      .then((response) => console.log(response));
+  }
+
   return (
     <Card>
       <div className="flex gap-2 p-4 pb ">
         <div>
-          <Avatar />
+          <Avatar url={profile?.avatar} />
         </div>
         <div className="relative w-full min-w-[200px] ">
-          <textarea
-            className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-gymGreen bg-transparent px-3 py-2.5 font-sans text-sm text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gymGreen focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
-            placeholder=" "
-          ></textarea>
+          {profile && (
+            <textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-gymGreen bg-transparent px-3 py-2.5 font-sans text-sm text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gymGreen focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+              placeholder=" "
+            ></textarea>
+          )}
+
           <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] leading-tight text-gymGray transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gymGreen peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gymGreen peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gymGreen peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-            What exercise is on your mind today?
+            What exercise is on your mind today, {profile?.name}?
           </label>
         </div>
       </div>
       <div className=" grow text-right mr-4 ">
-        <button className="bg-gymGreen text-black px-6 py-1 rounded-md mb-6">
+        <button
+          onClick={createPost}
+          className="bg-gymGreen text-black px-6 py-1 rounded-md mb-6"
+        >
           {" "}
           Post
         </button>
