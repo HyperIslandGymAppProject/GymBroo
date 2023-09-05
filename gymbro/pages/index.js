e03a4722c098b03f9164898a37174375322a818f
paramighty <avinnosatta@gmail.com>
2023-09-05 11:32:41 +0200

    Installed and setup necessary packages. Setup of Supabase.
    Co-authored-by: MorganRapaport <MorganRapaport@users.noreply.github.com>
    Co-authored-by: RebekkaBaerr <RebekkaBaerr@users.noreply.github.com>

--- gymbro/pages/index.js
+++ gymbro/pages/index.js
@@ -1,7 +1,16 @@
 import Layout from "../components/Layout";
 import PostFormCard from "../components/PostFormCard";
 import PostCard from "../components/PostCard";
+import { useSession } from "@supabase/auth-helpers-react";
+import LoginPage from "./login";
+
 export default function Home() {
+  const session = useSession();
+  console.log(session);
+
+  if (!session) {
+    return <LoginPage />;
+  }
   return (
     <Layout>
       <PostFormCard />
