37bb5458e8655c7e13e7f34906cfe1a872b0dc90
paramighty <avinnosatta@gmail.com>
2023-09-15 15:58:05 +0200

    Fixed the canvas error

--- gymbro/components/LoginCanvas.js
+++ gymbro/components/LoginCanvas.js
@@ -4,40 +4,24 @@ export default function LoginCanvas() {
   useEffect(() => {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
-
     document.body.appendChild(canvas);
     canvas.style.position = "fixed";
     canvas.style.top = 0;
     canvas.style.left = 0;
     canvas.style.zIndex = -100;
-
     const particles = [];
     const maxParticles = 20; // Adjust this value to control the number of particles
     let particleCounter = 0;
-
     // Load the image only once
     const img = new Image();
     img.src = "https://img.icons8.com/ios-glyphs/30/737373/dumbbell.png";
-
     img.onload = () => {
       animate(); // Start the animation after the image is loaded
     };
-    const maxParticles = 20; // Adjust this value to control the number of particles
-    let particleCounter = 0;
-
-    // Load the image only once
-    const img = new Image();
-    img.src = "https://img.icons8.com/ios-glyphs/30/737373/dumbbell.png";
-
-    img.onload = () => {
-      animate(); // Start the animation after the image is loaded
-    };
-
     function render() {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
       for (let i = 0; i < particles.length; i++) {
         const particle = particles[i];
         ctx.drawImage(
@@ -49,7 +33,6 @@ export default function LoginCanvas() {
         );
       }
     }
-
     function update() {
       // Limit the rate of adding particles
       if (particleCounter < maxParticles) {
@@ -60,31 +43,25 @@ export default function LoginCanvas() {
           speedX: Math.random() * 3 - 1,
           speedY: Math.random() * 3,
         };
-
         particles.push(particle);
         particleCounter++;
       }
-
       for (let i = 0; i < particles.length; i++) {
         const particle = particles[i];
         particle.x += particle.speedX;
         particle.y += particle.speedY;
-
         if (particle.x < 0 || particle.y > canvas.height) {
           particles.splice(i, 1);
           i--;
           particleCounter--; // Decrease the counter when a particle is removed
         }
       }
-
       render();
     }
-
     function animate() {
       update();
       requestAnimationFrame(animate);
     }
   }, []);
-
   return null;
 }
